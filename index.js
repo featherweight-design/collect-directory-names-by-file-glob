fb0b4d254e46103001970bc74d6d66f1e894d315
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-02-26 09:16:03 -0700

    chore: Remove unused logins

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const glob = require('glob');
 const path = require('path');
 
@@ -8,10 +7,9 @@ function findFromDirectory(searchDirectory, fileGlob) {
   const adjustedSearchPath = `${process.env.GITHUB_WORKSPACE}${searchDirectory}`
   console.log({ adjustedSearchPath });
   console.log(process.cwd())
-  console.log(path.resolve(__dirname, searchDirectory))
   console.log(path.resolve(__dirname, adjustedSearchPath))
 
-  const fileNames = glob.sync(fileGlob, { cwd: path.resolve(__dirname, searchDirectory) });
+  const fileNames = glob.sync(fileGlob, { cwd: path.resolve(__dirname, adjustedSearchPath) });
   console.log({ fileNames });
 
   const directoryNames = fileNames.map((fileName) =>
@@ -32,9 +30,6 @@ try {
 
   const directoryNames = findFromDirectory(searchDirectory, fileGlob);
   core.setOutput('directory-names', directoryNames);
-
-  const payload = JSON.stringify(github.context, undefined, 2);
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
