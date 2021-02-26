3f58149dbe567514f0224bfc19100f964a01d2c9
Hugh Hartigan <Hartigan.HM@gmail.com>
2021-02-26 07:46:54 -0700

    fix: Add logging to action index

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const glob = require('glob');
 const path = require('path');
 
 function findFromDirectory(searchDirectory, fileGlob) {
   const fileNames = glob.sync(fileGlob, { cwd: searchDirectory });
+  console.log({ fileNames });
+
   const directoryNames = fileNames.map((fileName) =>
     path.basename(path.dirname(fileName))
   );
+  console.log({ directoryNames });
 
   return directoryNames;
 }
@@ -16,8 +20,10 @@ try {
   const fileGlob = core.getInput('file-glob');
 
   const directoryNames = findFromDirectory(searchDirectory, fileGlob);
-
   core.setOutput('directory-names', directoryNames);
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
